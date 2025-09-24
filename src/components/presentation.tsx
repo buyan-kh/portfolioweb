a921c0428d632409b9fc2f6e0743df662a7e0ea0
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2025-09-23 23:30:10 -0700

    pics update

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -12,7 +12,7 @@ export function Presentation() {
     location: 'Mountain View, California',
     // Add a newline character after the emoji
     description: "Hey 👋\nI'm Buyan.",
-    src: '/profile-buyan.png',
+    src: '/profile-buyan.JPG',
     fallbackSrc:
       'https://images.unsplash.com/photo-1708873395735-dcad0140e3a2?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   };
